46d967fee2a68ba21653df375cef6657966f06bb
Mawan Sugiyanto <mawansugiyanto@Mawans-iMac.local>
2016-04-20 16:44:45 +0700

    update

--- components/MwAccessControl.php
+++ components/MwAccessControl.php
@@ -13,7 +13,20 @@ use yii\web\ForbiddenHttpException;
 
 class MwAccessControl extends ActionFilter
 {
+	public $user;
+	public $denyCallback;
 
+	public function init()
+    {
+        parent::init();
+        $this->user = Instance::ensure($this->user, User::className());
+        print_r($this->user);
+        // foreach ($this->rules as $i => $rule) {
+        //     if (is_array($rule)) {
+        //         $this->rules[$i] = Yii::createObject(array_merge($this->ruleConfig, $rule));
+        //     }
+        // }
+    }
 }
 
 ?>
