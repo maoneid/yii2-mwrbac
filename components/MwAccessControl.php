ca5289ce03227cd4d244ead2f78e7aedfa7b35eb
Mawan Sugiyanto <mawansugiyanto@Mawans-iMac.local>
2016-04-20 16:23:33 +0700

    Tambah component

---
+++ components/MwAccessControl.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace mw\mwrbac\components;
+
+use Yii;
+use yii\base\Action;
+use yii\base\ActionFilter;
+use yii\di\Instance;
+use yii\web\User;
+use yii\web\ForbiddenHttpException;
+
+
+
+class AccessControl extends ActionFilter
+{
+	
+}
+
+?>
