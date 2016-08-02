69cb2bfc0f8bf2498c988810af8fdd767a79370f
Mawan Sugiyanto <mawansugiyanto@Mawans-iMac.local>
2016-08-02 15:36:25 +0700

    update untuk proses cek login

--- components/MwAccessControl.php
+++ components/MwAccessControl.php
@@ -8,25 +8,100 @@ use yii\base\ActionFilter;
 use yii\di\Instance;
 use yii\web\User;
 use yii\web\ForbiddenHttpException;
-
+use yii\rbac\DbManager;
+use yii\web\UrlManager;
 
 
 class MwAccessControl extends ActionFilter
 {
-	public $user;
-	public $denyCallback;
+    public $user = "user";
+    public $userId = null;
+    public $denyCallback;
+    public $allowedActions = [];
+    public $rules = [];
 
-	public function init()
+    private $_authManager;
+    private $_urlManager;
+
+    public function init()
     {
         parent::init();
         $this->user = Instance::ensure($this->user, User::className());
-        print_r($this->user);
-        // foreach ($this->rules as $i => $rule) {
-        //     if (is_array($rule)) {
-        //         $this->rules[$i] = Yii::createObject(array_merge($this->ruleConfig, $rule));
+        $this->userId = $this->user->getId();
+
+        //load roles from auth manager 
+        $this->_authManager = Instance::ensure("authManager", DbManager::className());
+        $this->_urlManager = Instance::ensure("urlManager", UrlManager::className());
+        //$data = $this->_authManager->getPermissionsByUser($this->userId);
+
+        // print_r($data);
+        // exit;
+    }
+
+
+    public function beforeAction($action)
+    {
+        $user = $this->user;
+        $request = Yii::$app->getRequest();
+        $route = "";
+        echo Yii::$app->controller->getRoute();
+        echo "<br>";
+        $data = $this->_urlManager->parseRequest($request);
+        if (isset($data[0])){
+            $route = "/".$data[0];
+        }
+        echo $route;
+        if ($this->user->can($route)) {
+            return true;
+        }else{
+            // return true;
+            if ($action->id !== "error"){
+                $this->denyAccess($user);   
+            }else{
+                return true;
+            }
+            
+        }
+
+        return false;
+
+        // print_r($request);
+        // /* @var $rule AccessRule */
+        // foreach ($this->rules as $rule) {
+        //     if ($allow = $rule->allows($action, $user, $request)) {
+        //         return true;
+        //     } elseif ($allow === false) {
+        //         if (isset($rule->denyCallback)) {
+        //             call_user_func($rule->denyCallback, $rule, $action);
+        //         } elseif ($this->denyCallback !== null) {
+        //             call_user_func($this->denyCallback, $rule, $action);
+        //         } else {
+        //             $this->denyAccess($user);
+        //         }
+        //         return false;
         //     }
         // }
+        // if ($this->denyCallback !== null) {
+        //     call_user_func($this->denyCallback, null, $action);
+        // } else {
+        //     $this->denyAccess($user);
+        // }
+        // return false;
     }
+
+
+    protected function denyAccess($user)
+    {
+
+        if ($user->getIsGuest()) {
+            $user->loginRequired();
+        } else {
+            throw new ForbiddenHttpException(Yii::t('yii', 'You are not allowed to perform this action.'));
+        }
+    }
+
+
+
 }
 
 ?>
