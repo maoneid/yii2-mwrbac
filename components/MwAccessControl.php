eafe95eb06b905e54309cfac49d9eafacac9a7f2
Mawan Sugiyanto <mawansugiyanto@Mawans-iMac.local>
2016-11-08 18:40:46 +0700

    udpate ditambah dengan regex

--- components/MwAccessControl.php
+++ components/MwAccessControl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace mw\mwrbac\components;
 
@@ -10,21 +10,19 @@ use yii\web\User;
 use yii\web\ForbiddenHttpException;
 use yii\rbac\DbManager;
 use yii\web\UrlManager;
+use yii\rbac\Item;
 
+class MwAccessControl extends ActionFilter {
 
-class MwAccessControl extends ActionFilter
-{
     public $user = "user";
     public $userId = null;
     public $denyCallback;
     public $allowedActions = [];
     public $rules = [];
-
     private $_authManager;
     private $_urlManager;
 
-    public function init()
-    {
+    public function init() {
         parent::init();
         $this->user = Instance::ensure($this->user, User::className());
         $this->userId = $this->user->getId();
@@ -33,67 +31,44 @@ class MwAccessControl extends ActionFilter
         $this->_authManager = Instance::ensure("authManager", DbManager::className());
         $this->_urlManager = Instance::ensure("urlManager", UrlManager::className());
         //$data = $this->_authManager->getPermissionsByUser($this->userId);
-
         // print_r($data);
         // exit;
     }
 
+    public function beforeAction($action) {
 
-    public function beforeAction($action)
-    {
-        $user = $this->user;
-        $request = Yii::$app->getRequest();
-        $route = "";
-        echo Yii::$app->controller->getRoute();
-        echo "<br>";
-        $data = $this->_urlManager->parseRequest($request);
-        if (isset($data[0])){
-            $route = "/".$data[0];
+        $user = Yii::$app->user;
+        $authitem = $action->uniqueId;
+        $add_item = Yii::$app->params["add_auth_item"];
+        
+        if ($add_item) {
+            try {
+                $newPermission = $this->_authManager->createPermission($authitem);
+                $this->_authManager->add($newPermission);
+            } catch (\Exception $ex) {
+                Yii::error($ex->getMessage());
+            }
         }
-        echo $route;
-        if ($this->user->can($route)) {
-            return true;
-        }else{
-            // return true;
-            if ($action->id !== "error"){
-                $this->denyAccess($user);   
-            }else{
+        
+        if (is_array($this->allowedActions)){
+            if (in_array($authitem, $this->allowedActions)){
                 return true;
             }
-            
         }
-        
-        
 
+        if ($this->user->can($authitem)) {
+            return true;
+        } else {
+            if ($action->id !== "error") {
+                $this->denyAccess($user);
+            } else {
+                return true;
+            }
+        }
         return false;
-
-        // print_r($request);
-        // /* @var $rule AccessRule */
-        // foreach ($this->rules as $rule) {
-        //     if ($allow = $rule->allows($action, $user, $request)) {
-        //         return true;
-        //     } elseif ($allow === false) {
-        //         if (isset($rule->denyCallback)) {
-        //             call_user_func($rule->denyCallback, $rule, $action);
-        //         } elseif ($this->denyCallback !== null) {
-        //             call_user_func($this->denyCallback, $rule, $action);
-        //         } else {
-        //             $this->denyAccess($user);
-        //         }
-        //         return false;
-        //     }
-        // }
-        // if ($this->denyCallback !== null) {
-        //     call_user_func($this->denyCallback, null, $action);
-        // } else {
-        //     $this->denyAccess($user);
-        // }
-        // return false;
     }
 
-
-    protected function denyAccess($user)
-    {
+    protected function denyAccess($user) {
 
         if ($user->getIsGuest()) {
             $user->loginRequired();
@@ -101,8 +76,28 @@ class MwAccessControl extends ActionFilter
             throw new ForbiddenHttpException(Yii::t('yii', 'You are not allowed to perform this action.'));
         }
     }
-
-
+    
+    
+    protected function isActive($action){
+        
+        $actionUniqueId = $action->uniqueId;
+        $allAction = preg_replace(array("/\/\w+[\s\S]\w+$/"), array("/*"), $actionUniqueId);
+        $allModule = preg_replace(array("/\/\w+[\s\S]\w+\/\*$/"), array("/*"), $allAction);
+        
+        //for all module 
+        if (in_array($allModule, $this->allowedActions)){
+            return false;
+        }
+        
+        //for all action
+        if (in_array($allAction, $this->allowedActions)){
+            return false;
+        }
+        
+        
+        return true;
+        
+    }
 
 }
 
